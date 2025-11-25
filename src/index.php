e5eab81d5625976356884734f1d672baee0328a3
heldinho <hfpsis25@gmail.com>
2025-11-25 03:05:05 -0300

    Refactor JSON response handling in index.php by removing unnecessary JSON options for improved performance and consistency. Adjusted the placement of variable assignments for clarity.
    Signed-off-by: heldinho <hfpsis25@gmail.com>

--- src/index.php
+++ src/index.php
@@ -5,20 +5,20 @@ require_once __DIR__ . "/modules/person.php";
 header("Content-Type: application/json");
 
 $action = explode("/", $_SERVER["REQUEST_URI"])[1];
-$body = json_decode(file_get_contents("php://input"), true);
-$id = explode("/", $_SERVER["REQUEST_URI"])[2];
 
 switch ($action) {
   case "persons":
-    echo json_encode(Person::getAllPersons(), JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+    echo json_encode(Person::getAllPersons());
     break;
   case "person":
-    echo json_encode(Person::getPerson($id), JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+    $id = explode("/", $_SERVER["REQUEST_URI"])[2];
+    echo json_encode(Person::getPerson($id));
     break;
   case "create-person":
-    echo json_encode(Person::createPerson($body), JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+    $body = json_decode(file_get_contents("php://input"), true);
+    echo json_encode(Person::createPerson($body));
     break;
   default:
-    echo json_encode(["error" => "Action not found"], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+    echo json_encode(["error" => "Action not found"]);
     break;
 }
